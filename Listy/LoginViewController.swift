ef835fb4b517550a31acc0f8ca2c80141bdbefa6
Yagiz Nizipli <yagiznizipli@me.com>
2015-07-21 23:25:44 +0300

    Some minor fixes

--- Listy/LoginViewController.swift
+++ Listy/LoginViewController.swift
@@ -14,14 +14,13 @@ class LoginViewController: UIViewController {
     var avplayer: AVPlayer?
     
     override func viewDidLoad() {
-        
         self.navigationController?.navigationBarHidden = true;
         
         var error:NSError?
         
         AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryAmbient, error: &error)
         AVAudioSession.sharedInstance().setActive(true, error: &error)
-        //set up player
+
         var movieURL : NSURL? = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("listy", ofType: "mp4")!)
         var avAsset: AVAsset = AVAsset.assetWithURL(movieURL) as! AVAsset
         var avPlayerItem = AVPlayerItem(asset: avAsset)
@@ -35,23 +34,17 @@ class LoginViewController: UIViewController {
         movieView.layer.addSublayer(avPlayerLayer)
         self.view.addSubview(movieView)
         
-        //Config player
         
         self.avplayer?.seekToTime(kCMTimeZero)
         self.avplayer?.volume = 0
         self.avplayer?.actionAtItemEnd = AVPlayerActionAtItemEnd.None
+        
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "playerStartPlaying", name: UIApplicationDidBecomeActiveNotification, object: nil)
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "playerFinishedPlaying", name: AVPlayerItemDidPlayToEndTimeNotification, object: nil)
         
-        var gradient = CAGradientLayer()
-        gradient.frame = UIScreen.mainScreen().bounds
-        
-        var color1 = UIColor(red: 44/255, green: 62/255, blue: 80/255, alpha: 0.5)
-        var color2 = UIColor(red: 52/255, green: 73/255, blue: 94/255, alpha: 1)
-        
-        gradient.colors = [color1, color2, color1]
+        var whiteBackground = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 0.4)
         var gradientView = UIView(frame: self.view.frame)
-        gradientView.backgroundColor = color1
+        gradientView.backgroundColor = whiteBackground
         
         self.view.addSubview(gradientView)
         self.view.sendSubviewToBack(gradientView)
@@ -79,8 +72,6 @@ class LoginViewController: UIViewController {
     func login(){
         loginManager.logInWithReadPermissions(["email"], handler: { (result : FBSDKLoginManagerLoginResult!, error :NSError!) -> Void in
             if ((error) != nil || result.isCancelled) {
-                // Process error
-                
                 let alertController = UIAlertController(title: "Sorry", message:
                     "Failed to connect to Facebook servers", preferredStyle: UIAlertControllerStyle.Alert)
                 alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.Default,handler: nil))
@@ -93,7 +84,6 @@ class LoginViewController: UIViewController {
                 
                 println("error");
             } else {
-                // If you ask for multiple permissions at once, you should check if specific permissions missing
                 if (result.grantedPermissions.contains("email")){
                     self.sendFBTokens(result.token.tokenString);
                 }
@@ -108,7 +98,6 @@ class LoginViewController: UIViewController {
         httpRequest.HTTPBody = "{\"access_token\":\"\(access_token)\"}".dataUsingEncoding(NSUTF8StringEncoding);
         
         httpHelper.sendRequest(httpRequest, completion: {(data:NSData!, error:NSError!) in
-            // Display error
             if error != nil {
                 let errorMessage = self.httpHelper.getErrorMessage(error)
                 println(errorMessage);
